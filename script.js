a0b60e9a676aa6a94524382a39d6d168d7c4e98a
Francois <francois.thullier98@gmail.com>
2024-09-07 09:25:32 +0200

    Added the scores as DOM in HTML document, the winner is announced when n=5.

--- script.js
+++ script.js
@@ -1,47 +1,84 @@
 const btn=document.querySelectorAll("#btn");
+const container=document.querySelector("#container");
+
+// const text_div=document.querySelector("#result");
+const text=document.createElement("div");
+
+
+
 
 
 
 btn.forEach(box => box.addEventListener('click', () => {
     const humanSelection=box.textContent
     const computerSelection=getComputerChoice();
-    console.log('computer choice is : '+computerSelection);
+    // console.log('computer choice is : '+computerSelection);
 
     playRound(computerSelection,humanSelection);
-    console.log('human choice is : '+humanSelection);
+    // console.log('human choice is : '+humanSelection);
 }
     
      
   ));
+
+  
 
 
 let humanScore=0;
 let computerScore=0;
 
 function playRound(computerChoice,humanChoice) {
+        console.log(humanScore);
+        console.log(computerScore);
+        if(humanScore<5 && computerScore<5) {
 
-        // console.log(computerChoice);
-        // console.log(humanChoice);
-
-
-        if((computerChoice==="rock" && humanChoice==="scissors") || (computerChoice==="paper" && humanChoice==="rock") || (computerChoice==="scissors" && humanChoice==="paper") ) {
-            ++computerScore;
-            console.log("the human score is : ",humanScore,"\n and the computer score is : ",computerScore);
-            return(humanScore);
-        }
-
-        else if ( (humanChoice==="rock" && computerChoice==="scissors") || (humanChoice==="paper" && computerChoice==="rock") || (humanChoice==="scissors" && computerChoice==="paper") ) {
-            ++humanScore;
-            console.log("the human score is : ",humanScore,"\nand the computer score is : ",computerScore);
-            return(humanScore);
-            
+            if((computerChoice==="rock" && humanChoice==="scissors") || (computerChoice==="paper" && humanChoice==="rock") || (computerChoice==="scissors" && humanChoice==="paper") ) {
+                
+                // console.log("the human score is : ",humanScore,"\n and the computer score is : ",computerScore);
+                text.textContent="the human score is : "+humanScore+"\n and the computer score is : "+computerScore;
+                container.appendChild(text);
+                ++computerScore;
+                
+                return(humanScore);
+            }
+    
+            else if ( (humanChoice==="rock" && computerChoice==="scissors") || (humanChoice==="paper" && computerChoice==="rock") || (humanChoice==="scissors" && computerChoice==="paper") ) {
+                
+                // console.log("the human score is : ",humanScore,"\nand the computer score is : ",computerScore);
+                text.textContent="the human score is :\n "+humanScore+"\n and the computer score is : "+computerScore;
+                container.appendChild(text);
+                ++humanScore;
+                return(humanScore);
+                
+            }
+            else {
+                humanScore;
+                computerScore;
+    
+            }
         }
         else {
-            humanScore;
-            computerScore;
+            if(computerScore===5) {
+                text.textContent="computer won "+computerScore+" vs "+humanScore;
+                container.appendChild(text);
+            }
+            
+            else {
+                text.textContent="human won "+humanScore+" vs "+computerScore;
+                container.appendChild(text);
+        }
+            }
+            
+        
+         
+            
 
         }
-    }
+            
+        
+    
+        
+    
 
 function getComputerChoice() {
     num=Math.random();
